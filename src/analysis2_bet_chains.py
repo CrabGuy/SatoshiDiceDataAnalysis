725f8fffc9c19f42a1c22bfa11cb439f7e78b584
CrabGuy <bruno.direnzo2@gmail.com>
2026-06-19 10:08:54 +0200

    Finished analysis 2

--- src/analysis2_bet_chains.py
+++ src/analysis2_bet_chains.py
@@ -1,4 +1,34 @@
+import pandas
+import networkx
+
 from data_loader import read_parquet
+
+def build_transaction_graph(df: pandas.DataFrame) -> networkx.DiGraph:
+    G = networkx.DiGraph()
+    G.add_nodes_from(df["transaction_id"])
+
+    output_to_tx = dict(zip(df["change_id"], df["transaction_id"]))
+
+    edges = (
+        (output_to_tx[row.input_id], row.transaction_id)
+        for row in df.itertuples()
+        if row.input_id in output_to_tx
+    )
+
+    G.add_edges_from(edges)
+    return G
+
+def shortest_path_length_distribution(G: networkx.DiGraph) -> pandas.Series:
+    lengths = dict(networkx.all_pairs_shortest_path_length(G))
+
+    s = pandas.Series([
+        l
+        for source, targets in lengths.items()
+        for target, l in targets.items()
+        if l > 0
+    ])
+
+    return s.value_counts().sort_index()
 
 inputs = read_parquet("inputs")
 outputs = read_parquet("outputs")
@@ -37,4 +67,12 @@ simple_bets = (
     .reset_index()
 )
 
-print(simple_bets)
+MOST_POPULAR_DICE_ID = 3524394
+popular_simple_bets = simple_bets[simple_bets["dice_id"] == MOST_POPULAR_DICE_ID]
+
+G = build_transaction_graph(popular_simple_bets)
+
+path_length_distribution = shortest_path_length_distribution(G)
+
+# Plot this and fix up the rest of the plotting
+print(path_length_distribution)
